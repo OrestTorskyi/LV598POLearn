eba18278511e84d046f16fdfac9b805f69cecea5
Orest Torskyi <Orest.Torskyi.KN.2017@gmail.com>
2021-06-29 15:50:02 +0300

    Added test for verifying subscribing to newsletter

--- LV598POLearn/pages/base/BaseCommon.cs
+++ LV598POLearn/pages/base/BaseCommon.cs
@@ -1,9 +1,12 @@
-﻿using OpenQA.Selenium;
+﻿using LV598POLearn.utils;
+using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+
 
 namespace LV598POLearn.pages
 {
@@ -32,18 +35,34 @@ namespace LV598POLearn.pages
             return footerComponent.InputEmailAddressForNewsLetter(currentClass, text);
         }
 
+        public T clickOnSubmitEmailBtn<T>(T currentClass)
+        {
+            return footerComponent.clickOnSubmitEmailBtn(currentClass);
+        }
+
         public SignIn goToSignInPage()
         {
             return headerComponent.ClickOnSighInBtn();
         }
 
-        public MainPage goToMainPage()
+        public MainPage GoToMainPage()
         {
-            return headerComponent.goToMainPage();
+            return headerComponent.GoToMainPage();
         }
         public SearchQueryPage SearchFor(String text)
         {
             return headerComponent.SearchSomeProducts(text);
+        }
+
+        public T getAlertMessage<T>(T currentClass)
+        {
+            headerComponent.GetAlertMessage(currentClass);
+            return currentClass;
+        }
+
+        public string getTextOfAlertMessage()
+        {
+            return headerComponent.GetTextAlertMessage();
         }
         #endregion
     }
--- LV598POLearn/pages/base/HeaderComponent.cs
+++ LV598POLearn/pages/base/HeaderComponent.cs
@@ -14,8 +14,8 @@ namespace LV598POLearn.pages
         private IWebElement SignInBtn { get { return Driver.FindElement(By.XPath("//a[@class = 'login']")); } }
         private IWebElement InputSearchField { get { return Driver.FindElement(By.Id("search_query_top")); } }
         private IWebElement SubmitSearchQueryBtn { get { return Driver.FindElement(By.XPath("//button[@name = 'submit_search']")); } }
-
         private IWebElement ImageLogo { get { return Driver.FindElement(By.CssSelector(".logo.img-responsive")); } }
+        private IWebElement alertMessage;
         #endregion
         public HeaderComponent(IWebDriver driver) : base(driver) { }
 
@@ -32,10 +32,21 @@ namespace LV598POLearn.pages
             SubmitSearchQueryBtn.Click();
             return new SearchQueryPage(Driver);
         }
-        public MainPage goToMainPage()
+        public MainPage GoToMainPage()
         {
             ImageLogo.Click();
             return new MainPage(Driver);
+        }
+
+        public T GetAlertMessage<T>(T currentClass)
+        {
+            alertMessage = Driver.FindElement(By.XPath("//p[contains(@class, 'alert')]"));
+            return currentClass;
+        }
+
+        public String GetTextAlertMessage()
+        {
+            return alertMessage.Text;
         }
         #endregion
     }
