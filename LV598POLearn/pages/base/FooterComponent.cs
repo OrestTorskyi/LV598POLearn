7a140f3976938e6f6f29d4638d30b9ae13e554e9
Orest Torskyi <Orest.Torskyi.KN.2017@gmail.com>
2021-06-29 11:05:34 +0300

    some changes

---
+++ LV598POLearn/pages/base/FooterComponent.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV598POLearn.pages
+{
+    class FooterComponent : BasePage
+    {
+        #region Locators
+        private IWebElement NewsletterInput { get { return Driver.FindElement(By.Id("newsletter-input"));} }
+        private IWebElement EmailSubmitBtn { get { return Driver.FindElement(By.XPath("//div[contains(@id, 'newsletter')]//button[@type = 'submit']")); } }
+        #endregion
+        public FooterComponent(IWebDriver driver) : base(driver) { }
+
+        #region Methods
+        public T InputEmailAddressForNewsLetter<T>(T currentClass,String text)
+        {
+            NewsletterInput.Clear();
+            NewsletterInput.SendKeys(text);
+            EmailSubmitBtn.Click();
+            return currentClass;
+        }
+        #endregion
+
+    }
+}
---
+++ LV598POLearn/pages/base/HeaderComponent.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV598POLearn.pages
+{
+    class HeaderComponent : BasePage
+    {
+        #region Locators
+        private IWebElement ContactUsBtn { get { return Driver.FindElement(By.XPath("//div[contains(@id, 'contact')]/a")); } }
+        private IWebElement SignInBtn { get { return Driver.FindElement(By.XPath("//a[@class = 'login']")); } }
+        #endregion
+        public HeaderComponent(IWebDriver driver) : base(driver) { }
+
+        #region Methods
+        #endregion
+    }
+}
