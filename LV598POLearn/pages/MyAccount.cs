07dbda2041790d172d79ab5afa609d09e87dab32
VictoriaBadenko <victoria.badenko.vb@gmail.com>
2021-07-02 23:35:19 +0300

    Added class and test for saving personal information

--- LV598POLearn/pages/MyAccount.cs
+++ LV598POLearn/pages/MyAccount.cs
@@ -10,6 +10,7 @@ namespace LV598POLearn.pages
     public class MyAccount : BaseCommon
     {
         private IWebElement buttonMyWishlist { get { return Driver.FindElement(By.CssSelector("a[title='My wishlists']")); } }
+        private IWebElement buttonMyPersonalInformation { get { return Driver.FindElement(By.LinkText("Information")); } }
        
        public MyAccount(IWebDriver driver) : base(driver) { }
 
@@ -17,6 +18,12 @@ namespace LV598POLearn.pages
         {
             buttonMyWishlist.Click();
             return new WishListComponent(Driver);
+        }
+
+        public MyPersonalInformationPage clickOnMyPersonalInformation()
+        {
+            buttonMyPersonalInformation.Click();
+            return new MyPersonalInformationPage(Driver);
         }
     }
     
---
+++ LV598POLearn/tests/ChangePersonalInformationTest.cs
@@ -0,0 +1,40 @@
+﻿using LV598POLearn.pages;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV598POLearn.tests
+{
+    class ChangePersonalInformationTest : BaseTest
+    {
+        MyPersonalInformationPage myPersonalInformationPage;
+
+        [SetUp]
+        public void goToMyPersonalInformationWithLoggedInUser()
+        {
+            MainPage mainPage = new MainPage(driver);
+            mainPage.GetHeader();
+            MyAccount myAccount = mainPage.goToSignInPage().enterEmail().enterPassword().pressSignInButton();
+            myAccount.clickOnMyPersonalInformation();
+        }
+
+        [Test]
+        public void verifySavePersonalInformationChangesTest()
+        {
+            myPersonalInformationPage = new MyPersonalInformationPage(driver)
+               .clickOnSocialTitle()
+               .inputFirstName("Anna")
+               .inputLastName("Petruk")
+               .inputCurrentPassword("jkljkl")
+               .clickOnRecieveSpecialOffers()
+               .clickOnSaveButton();
+
+            string actualMessage = myPersonalInformationPage.getSaveMessage();
+            string expectedMessage = "Your personal information has been successfully updated.";
+            Assert.AreEqual(expectedMessage, actualMessage, "Sorry, your data wasn't save.Try again.");
+        }
+    }
+}
